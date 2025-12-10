a6747ca8c52d1fad3c194484fe329e796dc68284
I-FARIH <143896300+I-FARIH@users.noreply.github.com>
2025-12-10 22:22:39 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,8 +6,7 @@ db = SQLAlchemy()
 
 def init_db(app):
     """Initialize database with Flask app"""
-    password = quote_plus('Yas.Mel.5002')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{password}@localhost:5432/laundry_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///masbana.db'  # Use SQLite
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
